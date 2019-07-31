1b3aa46e67ebdbdf06372e607ed82f56eb4473dd
Vinh Tran <vincen7.tran@gmail.com>
2019-07-30 20:11:34 -0700

    album and artist pic render

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -6,18 +6,28 @@ import CardSection from './CardSection';
 
 const AlbumDetail = ({ album }) => {
   const { title, artist, image } = album;
-  const { headerContentStyle, imageStyle } = styles;
+  const thumbnail = 'http://newmediarockstars.com/wp-content/uploads/2015/06/TaylorSwift.png'
+  const {
+    headerContentStyle,
+    headerTextStyle,
+    thumbnailStyle,
+    thumbnailContainerStyle,
+    imageStyle
+  } = styles;
 
   return (
     <Card>
       <CardSection>
-        <View>
-          <Image style={imageStyle} source={{ uri: image }} />
+        <View style={thumbnailContainerStyle}>
+          <Image style={thumbnailStyle} source={{ uri: thumbnail }} />
         </View>
         <View style={headerContentStyle}>
-          <Text>{title}</Text>
+          <Text style={headerTextStyle}>{title}</Text>
           <Text>{artist}</Text>
         </View>
+      </CardSection>
+      <CardSection>
+        <Image style={imageStyle} source={{ uri: image }} />
       </CardSection>
     </Card>
   );
@@ -28,9 +38,23 @@ const styles = {
     flexDirection: 'column',
     justifyContent: 'space-evenly'
   },
-  imageStyle: {
+  headerTextStyle: {
+    fontSize: 18
+  },
+  thumbnailStyle: {
     height: 50,
     width: 50
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
   }
 };
 
