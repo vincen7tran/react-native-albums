d4915a3547a5325c9580628ad8d5a71bc36f5dba
Vinh Tran <vincen7.tran@gmail.com>
2019-07-30 20:03:16 -0700

    image and artist renders

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,17 +1,37 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Image, View, Text } from 'react-native';
 
 import Card from './Card';
 import CardSection from './CardSection';
 
 const AlbumDetail = ({ album }) => {
+  const { title, artist, image } = album;
+  const { headerContentStyle, imageStyle } = styles;
+
   return (
     <Card>
       <CardSection>
-        <Text>{album.title}</Text>
+        <View>
+          <Image style={imageStyle} source={{ uri: image }} />
+        </View>
+        <View style={headerContentStyle}>
+          <Text>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
       </CardSection>
     </Card>
   );
+};
+
+const styles = {
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-evenly'
+  },
+  imageStyle: {
+    height: 50,
+    width: 50
+  }
 };
 
 export default AlbumDetail;
