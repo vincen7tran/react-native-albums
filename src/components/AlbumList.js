4a33e24d568391648f5149dd464adae90d3c1517
Vinh Tran <vincen7.tran@gmail.com>
2019-07-30 19:07:11 -0700

    albumlists renders albums

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
 class AlbumList extends React.Component {
+  state = {
+    albums: []
+  }
+
+  componentDidMount() {
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(({ data }) => this.setState({ albums: data }));
+  }
+
+  renderAlbums = () => {
+    const { albums } = this.state;
+
+    return albums.map(album => {
+      const { title } = album;
+
+      return (
+        <Text key={title}>{title}</Text>
+      );
+    });
+  }
+
   render() {
     return (
       <View>
-        <Text>AlbumList</Text>
+        {this.renderAlbums()}
       </View>
     );
   }
