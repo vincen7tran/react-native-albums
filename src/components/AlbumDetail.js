d04c9886ef1c473136c0eb9ec2c73d5471c30b8c
Vinh Tran <vincen7.tran@gmail.com>
2019-07-30 19:40:51 -0700

    added cardsection component

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Text } from 'react-native';
+
+import Card from './Card';
+import CardSection from './CardSection';
 
 const AlbumDetail = ({ album }) => {
   return (
-    <View>
-      <Text>{album.title}</Text>
-    </View>
+    <Card>
+      <CardSection>
+        <Text>{album.title}</Text>
+      </CardSection>
+    </Card>
   );
 };
 
